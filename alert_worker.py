eb2636e3cc56ebcdab42cc9c77a747339968893f
natai44 <abdisanatai@gmail.com>
2026-04-16 10:13:40 +0200

    Update Telegram chat ID to use username format

--- alert_worker.py
+++ alert_worker.py
@@ -9,7 +9,7 @@ print("🔥 BOT FILE LOADED", flush=True)
 from signal_engine import get_multi_timeframe_analysis, evaluate_signal_engine, format_price
 
 TELEGRAM_BOT_TOKEN = "8785866877:AAHM-tze7VEOWcxGGcsVg0dWadheZX_Bhlw"
-TELEGRAM_CHAT_ID = "1080439188"
+TELEGRAM_CHAT_ID = "@magnet_0510"
 
 SCAN_SYMBOLS = [
     ("forex", "XAU/USD"),
